8d3790b40f30276d23f370f92908e85ab094acda
Corey Grandmaison <cgsustainability@gmail.com>
2019-07-18 18:31:46 -0400

    pushing everything to github and deploying everything

--- public/main.js
+++ public/main.js
@@ -1,7 +1,49 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let counter = 0;
+
+let teamOneScore = 0;
+let teamTwoScore = 0;
+
+const pageRefresh = () => {
+// set the text of both counters to the value of counter 
+  document.querySelector('.team-1-score').textContent = counter;
+  document.querySelector('.team-2-score').textContent = counter;
+};
+
+const addOneToCount = () => {  
+  counter += 1;
+  console.log("adding to counter", counter);
+  // update the HTML
+  // hey DOM, find the tag and update the content
+};
+
+const subtractOneFromCount = () => {  
+  counter -= 1;
+  console.log("subtracting from counter", counter);
+
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const updatingName = () => {
+  console.log("updating name!")
+}
+Document.querySelector('.team-1-input').addEventListener('blur', updatingName);
+
+
+//find the button, when it is clicked, do the thing
+document
+  .querySelector('.team-1-add-1-button')
+  .addEventListener('click', addOneToCount);
+
+document
+  .querySelector('.team-2-add-1-button')
+  .addEventListener('click', addOneToCount);
+
+document
+  .querySelector(".team-1-subtract-1-button")
+  .addEventListener('click', subtractOneFromCount);
+
+document
+  .querySelector(".team-2-subtract-1-button")
+  .addEventListener('click', subtractOneFromCount);
+
+
+document.addEventListener('DOMContentLoaded', pageRefresh); //when page is reloaded ('DOMContentLoaded') run pageRefrish function
