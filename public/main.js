1d117fbbc993baf5c6c8731d2cd9ef98329e01f7
Corey Grandmaison <cgsustainability@gmail.com>
2019-08-05 15:56:53 -0400

    updateing buttons and linking to events

--- public/main.js
+++ public/main.js
@@ -1,49 +1,76 @@
-let counter = 0;
-
+let zero = 0;
 let teamOneScore = 0;
 let teamTwoScore = 0;
 
 const pageRefresh = () => {
-// set the text of both counters to the value of counter 
-  document.querySelector('.team-1-score').textContent = counter;
-  document.querySelector('.team-2-score').textContent = counter;
+  // set the text of both counters to the value of counter
+  document.querySelector(".team-1-score").textContent = zero;
+  document.querySelector(".team-2-score").textContent = zero;
 };
 
-const addOneToCount = () => {  
-  counter += 1;
-  console.log("adding to counter", counter);
+const teamOneAddOne = () => {
+  teamOneScore += 1;
+  //make the .team-1-score update to the screen
+  document.querySelector(".team-1-score").textContent = teamOneScore;
+  console.log("added to Team One", teamOneScore);
   // update the HTML
   // hey DOM, find the tag and update the content
 };
 
-const subtractOneFromCount = () => {  
-  counter -= 1;
-  console.log("subtracting from counter", counter);
+const teamOneSubtractOne = () => {
+  teamOneScore -= 1;
+  //make the .team-1-score update to the screen
+  document.querySelector(".team-1-score").textContent = teamOneScore;
+  console.log("subtracted from Team One", teamOneScore);
+};
 
-}
+const teamTwoAddOne = () => {
+  teamTwoScore += 1;
+  console.log("added to Team Two", teamTwoScore);
+};
 
-const updatingName = () => {
-  console.log("updating name!")
-}
-Document.querySelector('.team-1-input').addEventListener('blur', updatingName);
+const teamTwoSubtractOne = () => {
+  teamTwoScore -= 1;
+  console.log("subtracted from Team Two", teamTwoScore);
+};
 
+const updateTeamOneName = () => {
+  document.querySelector(".team-1-name").textContent = document.querySelector(
+    ".team-1-input"
+  ).value;
+  console.log("updating Team One name!");
+};
+
+const updateTeamTwoName = () => {
+  document.querySelector(".team-2-name").textContent = document.querySelector(
+    ".team-2-input"
+  ).value;
+  console.log("updating Team Two name!");
+};
 
 //find the button, when it is clicked, do the thing
 document
-  .querySelector('.team-1-add-1-button')
-  .addEventListener('click', addOneToCount);
+  .querySelector(".team-1-add-1-button")
+  .addEventListener("click", teamOneAddOne); //done
 
 document
-  .querySelector('.team-2-add-1-button')
-  .addEventListener('click', addOneToCount);
+  .querySelector(".team-1-subtract-1-button") //done
+  .addEventListener("click", teamOneSubtractOne);
 
 document
-  .querySelector(".team-1-subtract-1-button")
-  .addEventListener('click', subtractOneFromCount);
+  .querySelector(".team-2-add-1-button")
+  .addEventListener("click", teamTwoAddOne);
 
 document
   .querySelector(".team-2-subtract-1-button")
-  .addEventListener('click', subtractOneFromCount);
+  .addEventListener("click", teamTwoSubtractOne);
 
+document //update team one name
+  .querySelector(".update-team-1-name")
+  .addEventListener("click", updateTeamOneName);
 
-document.addEventListener('DOMContentLoaded', pageRefresh); //when page is reloaded ('DOMContentLoaded') run pageRefrish function
+document //update team two name
+  .querySelector(".update-team-2-name")
+  .addEventListener("click", updateTeamTwoName);
+
+document.addEventListener("DOMContentLoaded", pageRefresh); //when page is reloaded ('DOMContentLoaded') run pageRefrish function
