6d9ce6baa62b44fff19950a62ec90d07a7678b50
Corey Grandmaison <cgsustainability@gmail.com>
2019-08-06 17:31:30 -0400

    Explorer is almost done. basic functionality in js

--- public/main.js
+++ public/main.js
@@ -3,45 +3,96 @@ let teamOneScore = 0;
 let teamTwoScore = 0;
 
 const pageRefresh = () => {
-  // set the text of both counters to the value of counter
-  // NOTE the Team names will reset automatically upon dom reload
+  // NOTE the Team names will reset automatically upon dom reload.
   document.querySelector(".team-1-score").textContent = zero;
   document.querySelector(".team-2-score").textContent = zero;
+  //take out the text in the input fields upon page refresh.
+  document.querySelector(".team-1-input").value = "";
+  document.querySelector(".team-2-input").value = "";
+  //when the page is reset add a hide attribute to the .classList
+  document.querySelector(".reset-button").classList.add("hide");
+};
+
+const resetGame = () => {
+  document.querySelector(".team-1-score").textContent = teamOneScore;
+  document.querySelector(".team-2-score").textContent = teamTwoScore;
+  console.log("Scores have been Reset!");
+  document.querySelector(".reset-button").classList.add("hide");
+  document.querySelector(".team-1-add-1-button").disabled = false;
+  document.querySelector(".team-2-add-1-button").disabled = false;
+  document.querySelector(".team-1-subtract-1-button").disabled = false;
+  document.querySelector(".team-2-subtract-1-button").disabled = false;
 };
 
 const teamOneAddOne = () => {
-  //change team one score
-  teamOneScore += 1;
-  // update the HTML, hey DOM, find the tag and update the content
-  document.querySelector(".team-1-score").textContent = teamOneScore;
-  //console.log the changes to the page, so I know they're working
-  console.log("added to Team One", teamOneScore);
+  //stop team score form going above 21
+  if (teamOneScore >= 21) {
+    console.log("Team One Won!!!");
+    document.querySelector(".team-1-name").append(" WON!");
+    //disable the button if score is 21
+    document.querySelector(".team-1-add-1-button").disabled = true;
+    //remove hide css attribute
+    document.querySelector(".reset-button").classList.remove("hide");
+    //reset var to 0
+    teamOneScore = 0;
+    teamTwoScore = 0;
+  } else {
+    //change team one score
+    teamOneScore += 1;
+    // update the HTML, hey DOM, find the tag and update the content
+    document.querySelector(".team-1-score").textContent = teamOneScore;
+    //console.log the changes to the page, so I know they're working
+    console.log("added to Team One", teamOneScore);
+  }
 };
 
 const teamOneSubtractOne = () => {
-  //change team one score
-  teamOneScore -= 1;
-  //hey DOM, find the tag and update the content
-  document.querySelector(".team-1-score").textContent = teamOneScore;
-  //console.log the changes to the page, so I know they're working
-  console.log("subtracted from Team One", teamOneScore);
+  //stop the score from going below 0
+  if (teamOneScore <= 0) {
+    console.log("Team One Score is 0, Cannot go Negative");
+  } else {
+    //change team one score
+    teamOneScore -= 1;
+    //hey DOM, find the tag and update the content
+    document.querySelector(".team-1-score").textContent = teamOneScore;
+    //console.log the changes to the page, so I know they're working
+    console.log("subtracted from Team One", teamOneScore);
+  }
 };
 
 const teamTwoAddOne = () => {
-  //add to team 2 score
-  teamTwoScore += 1;
-  // update team 2 score to the page.
-  document.querySelector(".team-2-score").textContent = teamTwoScore;
-  //console.lot the changes so I know you're working.
-  console.log("added to Team Two", teamTwoScore);
+  //stop team score from going above 21
+  if (teamTwoScore >= 21) {
+    console.log("Team Two Won!!!");
+    document.querySelector(".team-2-name").append(" Won!!!");
+    //disabling the add one button
+    document.querySelector(".team-2-add-1-button").disabled = true;
+    //show the reset button by classList .remove 'hide'
+    document.querySelector(".reset-button").classList.remove("hide");
+    //reset scores for both teams to 0
+    teamOneScore = 0;
+    teamTwoScore = 0;
+  } else {
+    //add to team 2 score
+    teamTwoScore += 1;
+    // update team 2 score to the page.
+    document.querySelector(".team-2-score").textContent = teamTwoScore;
+    //console.lot the changes so I know you're working.
+    console.log("added to Team Two", teamTwoScore);
+  }
 };
 
 const teamTwoSubtractOne = () => {
-  //subtract 1 from team 2 score
-  teamTwoScore -= 1;
-  //make team 2 score update to the page.
-  document.querySelector(".team-2-score").textContent = teamTwoScore;
-  console.log("subtracted from Team Two", teamTwoScore);
+  //if score is 0 print message and stop counter
+  if (teamTwoScore <= 0) {
+    console.log("Team  score is 0 and connot go negative");
+  } else {
+    //subtract 1 from team 2 score
+    teamTwoScore -= 1;
+    //make team 2 score update to the page.
+    document.querySelector(".team-2-score").textContent = teamTwoScore;
+    console.log("subtracted from Team Two", teamTwoScore);
+  }
 };
 
 const updateTeamOneName = () => {
@@ -86,5 +137,9 @@ document //update team one name
 document //update team two name
   .querySelector(".update-team-2-name")
   .addEventListener("click", updateTeamTwoName);
+
+document //working to reset the game by a button
+  .querySelector(".reset-button")
+  .addEventListener("click", resetGame);
 
 document.addEventListener("DOMContentLoaded", pageRefresh); //when page is reloaded ('DOMContentLoaded') run pageRefrish function
